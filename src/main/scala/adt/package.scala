86dcbfe061accd41d1f1b8c9752dc1552a8f9edd
Rafal Zelek <r.zelek@oberthur.com>
2018-11-05 23:15:27 +0100

    fixed bug in charWithFilter method

--- src/main/scala/adt/package.scala
+++ src/main/scala/adt/package.scala
@@ -1,21 +1,22 @@
 import scodec.Codec
-import scodec.codecs.{byte, provide, ubyte, vectorOfN}
+import scodec.codecs.{byte, provide, vectorOfN}
 import shapeless.{::, HNil}
 
 package object adt {
   def charWithFiller(maxLength: Byte): Codec[CharWithFiller] = {
     byte(8).consume { nameLength =>
       val name = vectorOfN(provide(nameLength), byte)
-      val nameFiller = vectorOfN(provide(38 - nameLength), byte)
+      val fillerLength = 38 - nameLength
+      val nameFiller = vectorOfN(provide(fillerLength), byte)
       (name :: nameFiller).xmap(toChar, toByte).as[CharWithFiller]
     }(_.value.length.toByte)
   }
 
   private def toChar(vectors: Vector[Byte] :: Vector[Byte] :: HNil): Vector[Char] :: Vector[Char] :: HNil = vectors match {
-    case a :: b :: HNil => a.map(_.toChar) :: a.map(_.toChar) :: HNil
+    case a :: b :: HNil => a.map(_.toChar) :: b.map(_.toChar) :: HNil
   }
 
   private def toByte(vectors: Vector[Char] :: Vector[Char] :: HNil): Vector[Byte] :: Vector[Byte] :: HNil = vectors match {
-    case a :: b :: HNil => a.map(_.toByte) :: a.map(_.toByte) :: HNil
+    case a :: b :: HNil => a.map(_.toByte) :: b.map(_.toByte) :: HNil
   }
 }
